f72ad11caeb03d202f77bb5c34e664c33a335d4a
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-05-31 11:00:13 +1000

    Changed files

--- portfolio/week 2/passwordCheck.sh
+++ portfolio/week 2/passwordCheck.sh
@@ -6,6 +6,11 @@ pr=$(basename $0)
 # Set the name of the secret file
 secretFile="secret.txt"
 
+# Get some colours
+red='\033[31m'
+green='\033[32m'
+resetColour='\033[0m'
+
 # Look for the secret file in the current directory. Note that there is no
 # conventional place for this file to exist, so we are defaulting to the current
 # working directory. If the file is not there, then exit with an error.
@@ -15,6 +20,7 @@ if [ ! -r "$secretFile" ]; then
 fi
 
 # Read the password in without terminal echo
+echo -en "$red"
 read -sp "Enter your password to check: " password
 echo
 
@@ -30,11 +36,11 @@ returnValue=$?
 if [ $returnValue -eq 0 ]; then
     # Check was successful. Echo the required message and exit with a 0 exit
     # code, indicating success.
-    echo "Access Granted"
+    echo -e "${green}Access Granted"
     exit 0
 fi
 
 # Check was unsuccessful. Echo the required message and exit with a 1 exit
 # code, indicating failure.
-echo "Access Denied"
+echo -e "${red}Access Denied"
 exit 1
