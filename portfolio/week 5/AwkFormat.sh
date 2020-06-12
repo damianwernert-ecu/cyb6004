e34d2891d063d2cb43ebc519a737ede0e86bfeef
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-06-12 11:12:39 +1000

    Added input.txt. AwkFormat modified

--- portfolio/week 5/AwkFormat.sh
+++ portfolio/week 5/AwkFormat.sh
@@ -2,6 +2,14 @@
 
 echo "Google Server IPs:"
 
-awk '{
-    print $1;
+awk 'BEGIN {
+    FS=":";
+    print "________________________________";
+    print "| \033[34mServer Type\033[0m | \033[34mIP\033[0m |";
+}
+{
+    printf("| \033[33m%-11s\033[0m | \033[35m%-14s\033[0m |\n", $1, $2);
+}
+END {
+    print("________________________________");
 }' input.txt
