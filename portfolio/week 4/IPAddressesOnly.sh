9fe594b31b436c445b8d985186d2e6e68d34df22
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-06-21 09:51:12 +1000

    Commented workings of IPAddressesOnly.sh

--- portfolio/week 4/IPAddressesOnly.sh
+++ portfolio/week 4/IPAddressesOnly.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# This script runs the IpIinfo.sh script and filters the output with sed. It uses the -n
+# option to supress printing by default, and then the "p" sed command to print lines that
+# match the string "IP Address"./
 ./IpInfo.sh  | sed -n '/IP Address/ p'
