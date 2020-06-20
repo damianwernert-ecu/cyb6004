c9b345edc189b2dce8133b55fcf65c83dafc6e24
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-06-21 09:52:58 +1000

    Added check for ./IpInfo.sh script

--- portfolio/week 4/IPAddressesOnly.sh
+++ portfolio/week 4/IPAddressesOnly.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+ipAddressScript="./IpInfo.sh"
+
+# Check that the ipAddressScript exists and is executable, and if not, error out.
+[ ! -x "$ipAddressScript" ] && echo "Script $ipAddressScript not found - exiting" >&2 && exit 1
+
 # This script runs the IpIinfo.sh script and filters the output with sed. It uses the -n
 # option to supress printing by default, and then the "p" sed command to print lines that
 # match the string "IP Address"./
-./IpInfo.sh  | sed -n '/IP Address/ p'
+$ipAddressScript  | sed -n '/IP Address/ p'
