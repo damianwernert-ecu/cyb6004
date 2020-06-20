ea58c7fbc7ab491c9f6597208a1eddaddfe332a8
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-06-21 09:57:10 +1000

    Extra comments in the awk section

--- portfolio/week 5/UserAccounts.sh
+++ portfolio/week 5/UserAccounts.sh
@@ -3,6 +3,7 @@
 # This is a bash wrapper around an awk script.
 
 awk -F: 'BEGIN {
+    # Define colours
     black = "\033[30m"
     red = "\033[31m"
     green = "\033[32m"
@@ -14,12 +15,15 @@ awk -F: 'BEGIN {
     bold = "\033[1m"
     resetColour = "\033[0m"
 
+    # This printf statement prints the headings
     printf("| %s%-17s%s| %s%-7s%s| %s%-8s%s| %s%-25s%s| %s%-18s%s|\n",
         blue, "Username", resetColour,
         blue, "UserID", resetColour,
         blue, "GroupID", resetColour,
         blue, "Home", resetColour,
         blue, "Shell", resetColour)
+
+    # This printf statemetn prints the underlines for the headings
     printf("| %-17s| %-7s| %-8s| %-25s| %-18s|\n",
         "_________________",
         "_______",
@@ -28,7 +32,10 @@ awk -F: 'BEGIN {
         "__________________")
 
 }
+
+# Match all lines that end in "/bin/bash", and run the following for each.
 /\/bin\/bash$/ {
+    # Prints the lines in /etc/passwd of interest.
     printf("| %s%-17s%s| %s%-7s%s| %s%-8s%s| %s%-25s%s| %s%-18s%s|\n",
         green, $1, resetColour,
         green, $3, resetColour,
@@ -41,4 +48,3 @@ END {
 
 # Exit with the same code as awk does.
 exit $?
-
