13dfa9586956005e1794ec4b90dd1ba225186960
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-06-21 22:29:04 +1000

    Overhauled cache update code

--- assignment/vulnUpdates.sh
+++ assignment/vulnUpdates.sh
@@ -5,7 +5,13 @@
 umask 0177
 
 # Identify the program name for later use
-program=$(basename $0)
+PROGRAM=$(basename $0)
+
+# Initialize other variables
+DATA_URL="https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.gz"
+META_URL="https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.meta"
+DATA_FILE="$HOME/.${PROGRAM}.data.gz"
+MAX_CACHE_AGE=2   # hours
 
 # Define the usage string for errors in invocation
 usage="Usage: $pr [OPTIONS]
@@ -27,24 +33,20 @@ OPTIONS include:
         Supress reporting of NVD entries that have an unknown vulnerability score
 "
 
-# Initialize other variables
-dataURL="https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.gz"
-dataFile="$HOME/.${program}.data.gz"
-
 ## Define functions
 
 # ExtendedHelp is a self-made man page that give the user more detailed information about the script.
 ExtendedHelp() {
     sed 's/^ *//' <<-!EOD | man -l -
-    .\" Manpage for $program
+    .\" Manpage for $PROGRAM
     .\" Contact dwernert@our.ecu.edu.au for feedback.
-    .TH $program 1 "16 June 2020" "1.0" "$program manual page"
+    .TH $PROGRAM 1 "16 June 2020" "1.0" "$PROGRAM manual page"
     .SH NAME
-    $program \- display recently discovered vulnerabilities
+    $PROGRAM \- display recently discovered vulnerabilities
     .SH SYNOPSIS
-    $program [OPTIONS]
+    $PROGRAM [OPTIONS]
     .SH DESCRIPTION
-    $program is a program that retrives a list of recently updated vulnerabilities from the National Vulnerability Database
+    $PROGRAM is a program that retrives a list of recently updated vulnerabilities from the National Vulnerability Database
     (NVD). The user may determine how recently to report on the vulnerability updates. Updates are capped at a maximum of 8 days
     old, but may include vulnerabilities report well before that date, that have been updated within the last 8 days.
     .SH OPTIONS
@@ -71,7 +73,7 @@ ExtendedHelp() {
     .IP
     Suppress reporting of unknown vulnerability scores.
     .SH NOTES
-    The program will create a cache file in $dataFile where it will store recent data. The NVD is updated every
+    The program will create a cache file in $DATA_FILE where it will store recent data. The NVD is updated every
     two hours and datestamped, so that if the datestamp is more than 2 hours out-of-date, a new copy will be automatically
     downloaded.  A cache update may be forced by using the -c option.
     .LP
@@ -82,7 +84,107 @@ ExtendedHelp() {
     .SH AUTHOR
     Damian Wernert (dwernert@our.ecu.edu.au)
 	!EOD
+    # Note that on the preceding line, it is specifically indented with a tab, and not spaces. This is to make the indented
+    # !EOD work.
     return $?
+}
+
+RetrieveMeta() {
+    local rawMetadata=$(wget -qO - "$META_URL")
+    local modifiedEpoch=$(date +%s -d"$(echo \"$rawMetadata\" | grep \"^lastModifiedDate:\" | cut -f2 -d:)")
+    local fileSize=$(echo "$rawMetadata" | grep "^zipSize:" | cut -f2 -d:)
+    local sha256=$(echo "$rawMetadata" | grep "^sha256:" | cut -f2 -d:)
+
+    # Make sure we have some values
+    if [ -z "$modifiedEpoch" -o -z "$fileSize" -o -z "$sha256" ]; then
+        echo "$PROGRAM: failed to retrieve metadata" >&2
+        return 1
+    fi
+
+    # "return" the data by stdout
+    echo "$modifiedEpoch $fileSize $sha256"
+    return 0
+}
+
+DownloadData() {
+    local fileName
+    local fileSize
+    local checksum
+
+    # Download data and compare with sizes and checksum
+    wget -q -O "$fileName" "$DATA_URL"
+    downloadSize=$(stat -c %s "$fileName")
+    if [ "$downloadSize" -eq "$fileSize" ]; then
+        fileChecksum=$(sha256sum "$fileName" | awk '{print $1}')
+        if [ "$fileChecksum" -eq "$checksum" ]; then
+            echo "$PROGRAM: download successful"
+            return 1
+        else
+            echo "$PROGRAM: file checksum ($fileChecksum) incorrect. Expected: $checksum" >&2
+            return 0
+        fi
+    else
+        echo "$PROGRAM: download file was the wrong size. Was: $downloadSize bytes, expected $fileSize" >&2
+        return 0
+    fi
+}
+
+UpdateCache() {
+    local dataFile="$1"
+    local forceUpdate="$2"
+    local maxAge="$3"   # Hours
+    local dataDateString
+    local dataDateEpoch
+    local metaDate
+    local gzipSize
+    local sha256checksum
+    local ageCutoff=$(date +%s -d"${maxAge} hours ago")
+    local getDownload=no
+
+    # We must minimise the the number of downloads. The NVD has two URLs in relation to the data:
+    # 1. the metadata URL (which holds update time, sizes and checksum information); and
+    # 2. the actual data URL.
+    # The metadata can be used to both:
+    # 1. verify a download; and
+    # 2. to examine if newer data exists for a download to occur.
+
+    # Metadata is required for all circumstances except for one: when our datafile exists, its data is recent enough, and
+    # the user hasn't forced a download with the -c option. We will check for this condition first:
+    if [ -r "$dataFile" ]; then
+        dataDateString=$(gunzip -c "$dataFile" | jq -r '."CVE_data_timestamp"')
+        dataDateEpoch=$(date +%s -d"$dataDateString")
+
+        # If the data is recent, then no download is needed, unless the user forced the download.
+        if [ "$forceUpdate" = "no" ]; then
+            if [ $dataDateEpoch -gt $ageCutoff ]; then
+                echo "$PROGRAM: our data is less than $maxAge hours old - no download needed"
+                return 0
+            fi
+        fi
+    fi
+
+    # From now on, we need the metadata provided by the NVD about new downloads.
+    if ! RetrieveMeta | read metaDate gzipSize sha256checksum; then
+        return 1
+    fi
+
+    # If there is no datafile, or an update is forced, then we download data, using the metadata to verify the download.
+    if [ ! -r "$dataFile" ] || [ "$forceUpdate" = "yes" ]; then
+        echo "$PROGRAM: downloading new data"
+        DownloadData "$dataFile" "$gzipSize" "$sha256checksum" && return 0
+        return 1
+    fi
+
+    # If we get this far, the datafile must exist, and it is old. We look at the metadata's last update to see if we should
+    # download new data.
+    if [ $metaDate -le $dataDateEpoch ]; then
+        echo "$PROGRAM: downloading new data"
+        DownloadData "$dataFile" "$gzipSize" "$sha256checksum" && return 0
+        return 1
+    else
+        echo "$PROGRAM: no new data on the NVD server"
+        return 0
+    fi
 }
 
 # Default values affected my command-line options
@@ -115,53 +217,38 @@ shift $(( OPTIND - 1 ))
 # Convert earliestTimeString into an epoch time
 earliestTime=$(date +%s -d"$earliestTimeString")
 if [ $? -ne 0 ]; then
-    echo "$program: the date string provided is invalid" >&2
+    echo "$PROGRAM: the date string provided is invalid" >&2
     exit 2
 fi
 
 # Validate the input of minSeverity
 if ! [[ $minSeverity =~ ^[0-9]*\.?[0-9]*$ ]]; then
-    echo "$program: minimum severity is invalid" >&2
+    echo "$PROGRAM: minimum severity is invalid" >&2
     exit 3
 fi
 
 # Find out if we have a data file, and if we need to update it
-if  [ -r "$dataFile" ]; then
-    # Find out how recent the data is
-    dataDateString=$(gunzip -c "$dataFile" | jq -r '."CVE_data_timestamp"')
-    dataDateEpoch=$(date +%s -d"$dataDateString")
-    if [ $dataDateEpoch -le $(date +%s -d"2 hours ago") ]; then
-        echo "$program: cached data is old - requesting update"
-        updateCache=yes
-    fi
-else
-    updateCache=yes
-fi
-
-# Now update the data file cache if necessary
-if [ "$updateCache" = yes ]; then
-    echo "$program: updating data cache"
-    #wget -q -O "$dataFile" "$dataURL"
-else
-    echo "$program: using cached data"
+if ! UpdateCache "$DATA_FILE" "$updateCache"; then
+    echo "$PROGRAM: data download or update failed" >&2
+    exit 4
 fi
 
 # Create a list of indexes corresponding to the array provided by the NVD.
-arrayIndexes=$(gunzip -c "$dataFile"  | jq '."CVE_Items" | keys | .[]')
+arrayIndexes=$(gunzip -c "$DATA_FILE"  | jq '."CVE_Items" | keys | .[]')
 lastIndex=$(echo "$arrayIndexes" | tail -1)
 
 # Record the number of reported vulnerabilites
 numHits=0
 
 # Display parameter data
-echo "$program: searching through $lastIndex vulnerabilities for scores of $minSeverity or higher"
+echo "$PROGRAM: searching through $lastIndex vulnerabilities for scores of $minSeverity or higher since $(date -d@${earliestTime})"
 
 # Iterate through the array of vulnerabilities
 for vuln in $arrayIndexes; do
     echo -ne "Searching $vuln of $lastIndex\r"
 
     # Retrieve the last modified date, and convert it to epoch time
-    lastModifiedDateString=$(gunzip -c "$dataFile" | jq -r ".CVE_Items[${vuln}].lastModifiedDate")
+    lastModifiedDateString=$(gunzip -c "$DATA_FILE" | jq -r ".CVE_Items[${vuln}].lastModifiedDate")
     lastModifiedDateEpoch=$(date +%s -d"$lastModifiedDateString")
 
     # Find out if the lastModifiedDate is late enough to be listed
@@ -171,9 +258,9 @@ for vuln in $arrayIndexes; do
     fi
 
     # Retrieve the impact score of the vulnerability, and fall back to V2 if necessary
-    score=$(gunzip -c "$dataFile" | jq ".CVE_Items[${vuln}].impact.baseMetricV3.cvssV3.baseScore")
+    score=$(gunzip -c "$DATA_FILE" | jq ".CVE_Items[${vuln}].impact.baseMetricV3.cvssV3.baseScore")
     if [ -z "$score" ]; then
-        score=$(gunzip -c "$dataFile" | jq ".CVE_Items[${vuln}].impact.baseMetricV2.cvssV2.baseScore")
+        score=$(gunzip -c "$DATA_FILE" | jq ".CVE_Items[${vuln}].impact.baseMetricV2.cvssV2.baseScore")
     fi
 
     # Find out if the score is greater than or equal to the score requested by the user
@@ -191,18 +278,18 @@ for vuln in $arrayIndexes; do
     fi
 
     # Retrieve the vulnerability ID
-    vulnID=$(gunzip -c "$dataFile" | jq ".CVE_Items[${vuln}].cve.CVE_data_meta.ID")
+    vulnID=$(gunzip -c "$DATA_FILE" | jq ".CVE_Items[${vuln}].cve.CVE_data_meta.ID")
 
     # Retrieve the vulnerability description. The -r option on jq removes the surrounding quotes
-    description=$(gunzip -c "$dataFile" | jq -r ".CVE_Items[${vuln}].cve.description.description_data | .[].value")
+    description=$(gunzip -c "$DATA_FILE" | jq -r ".CVE_Items[${vuln}].cve.description.description_data | .[].value")
 
     # Display the vulnerability data
     printf "%d: %s score = %s (Last updated %s)\n" $vuln "$vulnID" "$displayScore" "$lastModifiedDateString"
     echo "    Description:"
-    echo "$description" | fold -s | awk '{print "        " $0 }'
+    echo -e "$description\n" | fold -s | awk '{print "        " $0 }'
 
     # Update the number of reported vulnerabilities
     (( numHits ++ ))
 done
 
-echo "$program: $numHits hits reported"
+echo "$PROGRAM: $numHits hits reported"
