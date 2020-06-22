e66fb8f42af64127485ba4dd71e7f25246dd2d06
damianwernert-ecu <dwernert@our.ecu.edu.au>
2020-06-22 10:01:36 +1000

    Added argument check and usqage

--- portfolio/week 1/Second.sh
+++ portfolio/week 1/Second.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Program to echo a given argument to the user.
+# Program to echo the first given argument to the user.
+
+# Check that the user has supplied an argument.
+if [ -z "$1" ]; then
+    echo "Usage: $0 name" >&2
+    exit 1
+fi
 
 echo "Hi there!"
 echo "It's good to see you $1!"
